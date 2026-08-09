6e17b72fc5e1e6931581f2995209ce850655e9e3
VinChan2001 <95763053+VinChan2001@users.noreply.github.com>
2026-08-09 19:32:03 -0400

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.py
@@ -0,0 +1,21 @@
+# class Solution:
+#     def hasDuplicate(self, nums: List[int]) -> bool:
+#         for i in range(len(nums)):
+#             if nums[i] in nums[i+1:]:
+#                 return True
+#         return False
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(0, len(nums)-1):
+            if nums[i]==nums[i+1]:
+                return True 
+        return False
+# class Solution:
+#     def hasDuplicate(self, nums: List[int]) -> bool:
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i]==nums[j]:
+#                     return True
+#         return False
