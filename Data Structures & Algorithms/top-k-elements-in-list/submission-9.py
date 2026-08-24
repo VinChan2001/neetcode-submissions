7271df93dd1b5698c9718ce1e77ee55f6ad379a5
VinChan2001 <95763053+VinChan2001@users.noreply.github.com>
2026-08-23 21:01:42 -0400

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # k most frequent elements
+        d1= defaultdict(int)
+        for i in nums:
+            d1[i]+=1
+        
+        p=0
+        l=[]
+        
+        while p<k and d1:
+            maxi=0
+            maxi_i=-1
+            for i, j in d1.items():
+                if j>maxi:
+                    maxi=j
+                    maxi_i=i
+            p+=1
+            l.append(maxi_i)
+            d1.pop(maxi_i)
+        return l
+
+
+            
+
+        
