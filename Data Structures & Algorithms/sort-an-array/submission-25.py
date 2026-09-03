3dcc8b2bb98555a3d1d08ce216183fe8f2df03eb
VinChan2001 <95763053+VinChan2001@users.noreply.github.com>
2026-09-03 18:11:26 -0400

    Add: sort-an-array - submission-25

---
+++ Data Structures & Algorithms/sort-an-array/submission-25.py
@@ -0,0 +1,33 @@
+import random
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.quick(nums, 0, len(nums)-1)
+        return nums
+
+    def quick(self, nums, low, high):
+        if low>=high:
+            return
+        
+        pivot_index = self.partition(nums, low, high)
+
+        self.quick(nums, low, pivot_index)
+        self.quick(nums, pivot_index+1, high)
+
+    def partition(self, nums, low, high):
+        pivot_index = random.randint(low, high)
+
+        nums[high], nums[pivot_index] = nums[pivot_index], nums[high]
+        pivot = nums[high]
+        i = low
+
+        for j in range(low, high):
+            if nums[j]<pivot:
+                nums[i], nums[j] = nums[j], nums[i]
+                i+=1
+        
+        nums[i], nums[high] = nums[high], nums[i]
+        
+        return i
+
+
+        
